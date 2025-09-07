1283720bf6959a9292ff22b8f73840ddba93c071
Branpedia <162459839+branpedia@users.noreply.github.com>
2025-09-07 12:52:56 +0700

    Update tofigure.js

--- api/tofigure.js
+++ api/tofigure.js
@@ -6,7 +6,7 @@ const APIKEY = 'AIzaSyCViCU51ps2_XVNBGz4LbktGYgy0yuU1Io';
 const PROMPT = 'Using the nano-banana model, a commercial 1/7 scale figurine of the character in the picture was created, depicting a realistic style and a realistic environment. The figurine is placed on a computer desk with a round transparent acrylic base. There is no text on the base. The computer screen shows the Zbrush modeling process of the figurine. Next to the computer screen is a BANDAI-style toy box with the original painting printed on it.';
 
 // Konfigurasi GitHub
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "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";
 const GITHUB_USER = "kepocodeid";
 const GITHUB_REPO = "testeraja";
 
