6355c2bb36d5f9ad38e336341ee54cd2c8f8e53d
Branpedia <162459839+branpedia@users.noreply.github.com>
2025-09-11 14:48:09 +0700

    Update tofigure.js

--- api/tofigure.js
+++ api/tofigure.js
@@ -2,7 +2,7 @@ import cloudscraper from 'cloudscraper';
 import { JSDOM } from 'jsdom';
 import puppeteer from 'puppeteer';
 
-const APIKEY = 'AIzaSyA3Qw0N_mOXnFHzaTSHEkQ6FtDS4AjfzjE';
+const APIKEY = 'AIzaSyCebxtx7AdHqfZKYx0JLimlJrKOPwGbZIQ';
 const PROMPT = 'Using the nano-banana model, a commercial 1/7 scale figurine of the character in the picture was created, depicting a realistic style and a realistic environment. The figurine is placed on a computer desk with a round transparent acrylic base. There is no text on the base. The computer screen shows the Zbrush modeling process of the figurine. Next to the computer screen is a BANDAI-style toy box with the original painting printed on it.';
 
 // Function untuk call Google Gemini API menggunakan cloudscraper
